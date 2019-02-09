b2e46837f99fb57b71171bc7517ddd373117ed9e
HPCDevOPS <hpcdevops@users.noreply.github.com>
2019-02-08 16:16:00 -0800

    Major rewrite of rocksbuilder.sh

--- usr/local/bin/rocksbuilder.sh
+++ usr/local/bin/rocksbuilder.sh
@@ -1,13 +1,48 @@
 #!/bin/bash
 
-function finish {
-    echo "im done here"
+trap _rb_finish EXIT
+
+version='0.1.0'
+
+. ./rocksbuilder_functions.sh
+
+my_path=$(_rb_abspath "$0")
+my_dir=$(dirname "${my_path}")
+my_name=$(basename "${my_path}")
+my_date=$(date )
+
+readonly version
+readonly my_path
+readonly my_dir
+readonly my_name
+readonly my_date
+
+export PATH=$PATH:/bin:/sbin:/usr/bin:/usr/local/bin:/usr/sbin
+
+. ${my_dir}/../../../etc/profile.d/rocks-binaries.sh
+. ${my_dir}/../../../etc/profile.d/rocks-devel.sh
+
+usage () {
+	cat <<EOF
+  usage: ${my_name}
+EOF
 }
 
-trap finish EXIT
+while getopts h args
+do
+	case $args in
+	h) usage; exit 0 ;;
+	*) usage; exit 1 ;;
+	esac
+done
 
-chown -R rocksdb:rocksdb /var/opt/rocks/mysql
+main() {
+	for test in $(find "${my_dir}/tests" -name "*.sh"); do
+		. "$test"
+	done
 
-echo "$(date +%H:%M:%S) Finished initial startup"
+	printf "\n"
+	echo "${my_date} Finished initial startup"
+}
 
-exec "$@"
+main "$@"
